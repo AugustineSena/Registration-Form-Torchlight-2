e2eaaf970ebd166634fdc7a0025f721afb8df063
ꦱꦺꦤ <bohaswarasena@gmail.com>
2019-04-02 23:39:22 +0700

    Creating compact enumeration
    cleaning code

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,6 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import javax.swing.*;
@@ -13,23 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
         // TODO code application logic here
         RegistrationForm createNew = new RegistrationForm();
-        createNew .setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        createNew.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         createNew .setSize( 900, 450 ); // set frame size
         createNew .setVisible( true ); // display frame
         createNew.setResizable(false);
-        /*  try {
-    		createNew.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("src/register/background_full.jpg")))));
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    	}
-    	createNew.pack();*/
-
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
         int x = (int) ((dimension.getWidth() - createNew.getWidth()) / 2);
         int y = (int) ((dimension.getHeight() - createNew.getHeight()) / 2);
